412271c657c1825f3c211f8c8420d45648ff9daa
Jack Anthony <jackanth3@gmail.com>
2018-01-31 16:36:45 +0000

    Temporary fix to CheatingNeutrinoIdTool.

--- larpandoracontent/LArCheating/CheatingNeutrinoIdTool.cc
+++ larpandoracontent/LArCheating/CheatingNeutrinoIdTool.cc
@@ -60,6 +60,86 @@ void CheatingNeutrinoIdTool::SelectOutputPfos(const pandora::Algorithm *const /*
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
+void CheatingNeutrinoIdTool::GetNeutrinoWeight(const CaloHit *const pCaloHit, float &neutrinoWeight, float &totalWeight) const
+{
+    neutrinoWeight = 0.f; totalWeight = 0.f;
+    
+    const CaloHit *const pCaloHitMaster(reinterpret_cast<const CaloHit *>(pCaloHit->GetParentAddress()));
+    
+    if (!pCaloHitMaster)
+    {
+        std::cout << "CheatingNeutrinoIdTool: failed to cast CaloHit parent address as CaloHit" << std::endl;
+        return;
+    }
+    
+    const MCParticleWeightMap &hitMCParticleWeightMap(pCaloHitMaster->GetMCParticleWeightMap());
+
+    if (hitMCParticleWeightMap.empty())
+        return;
+    
+    MCParticleList mcParticleList;
+    for (const auto &mapEntry : hitMCParticleWeightMap) mcParticleList.push_back(mapEntry.first);
+    mcParticleList.sort(LArMCParticleHelper::SortByMomentum);
+
+    for (const MCParticle *const pMCParticle : mcParticleList)
+    {
+        const float weight(hitMCParticleWeightMap.at(pMCParticle));
+
+        if (LArMCParticleHelper::IsBeamNeutrinoFinalState(pMCParticle))
+            neutrinoWeight += weight;
+
+        totalWeight += weight;
+    }
+    
+    // ATTN normalise arbitrary input weights at this point
+    if (totalWeight > std::numeric_limits<float>::epsilon())
+    {
+        neutrinoWeight *= 1.f / totalWeight;
+        totalWeight = 1.f;
+    }
+    else
+    {
+        neutrinoWeight = 0.f;
+        totalWeight = 0.f;
+    }
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void CheatingNeutrinoIdTool::GetNeutrinoWeight(const PfoList *const pPfoList, float &neutrinoWeight, float &totalWeight) const
+{
+    neutrinoWeight = 0.f; totalWeight = 0.f;
+
+    for (const ParticleFlowObject *const pPfo : *pPfoList)
+    {
+        ClusterList twoDClusters;
+        LArPfoHelper::GetTwoDClusterList(pPfo, twoDClusters);
+        
+        CaloHitList caloHitList;
+        
+        for (const Cluster *const pCluster : twoDClusters)
+        {
+            const HitType hitType(LArClusterHelper::GetClusterHitType(pCluster));
+
+            if ((TPC_VIEW_U != hitType) && (TPC_VIEW_V != hitType) && (TPC_VIEW_W != hitType))
+                throw StatusCodeException(STATUS_CODE_INVALID_PARAMETER);
+
+            pCluster->GetOrderedCaloHitList().FillCaloHitList(caloHitList);
+        }
+        
+        for (const CaloHit *const pCaloHit : caloHitList)
+        {
+            float thisNeutrinoWeight = 0.f, thisTotalWeight = 0.f;
+            this->GetNeutrinoWeight(pCaloHit, thisNeutrinoWeight, thisTotalWeight);
+            
+            neutrinoWeight += thisNeutrinoWeight;
+            totalWeight += thisTotalWeight;
+        }
+    }
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
 StatusCode CheatingNeutrinoIdTool::ReadSettings(const TiXmlHandle /*xmlHandle*/)
 {
     return STATUS_CODE_SUCCESS;
